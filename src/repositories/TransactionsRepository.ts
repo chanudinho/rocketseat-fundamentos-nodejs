d08e6641f98b6a427788cbb86afdaff051d9d106
Antonio Neto <chanudinho@hotmail.com>
2020-04-27 11:49:46 -0300

    desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,54 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    if (type === 'income') {
+      this.balance = {
+        income: this.balance.income + value,
+        outcome: this.balance.outcome,
+        total: this.balance.total + value,
+      };
+    } else {
+      this.balance = {
+        income: this.balance.income,
+        outcome: this.balance.outcome + value,
+        total: this.balance.total - value,
+      };
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
